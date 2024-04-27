823b96b4af80258f2cf536d5b2bafd6318a688c4
jonathan <jonathanxavier556@gmail.com>
2024-04-27 19:12:41 -0300

    primeiro commit - criando o site do meu casamento

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div>
+    <h1>Jamy memes</h1>
+  </div>
+  <div id="carouselExample" class="carousel slide">
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./assets/jamy2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/jamy.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/jamty3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</template>
+
+<script >
+
+export default{
+  data:function(){
+    return {
+      code: '',
+    }
+  },
+
+  computed:{
+
+  },
+
+  methods:{
+    teste(){
+      alert('funciona')
+    }
+
+  },
+
+  mounted(){
+
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/config/bootstrap.js
@@ -0,0 +1,5 @@
+// import Vue from 'vue'
+// import {, IconsPlugin} from 'bootstrap'
+
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
